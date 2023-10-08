8ba07d894560991d0379dd432c2568d29b783e05
Andi-gech <andi.fab23@gmail.com>
2023-10-09 02:07:13 +0300

    update

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Product,Catagory,CustomerProfile,Orders,OrderItems,News,SubCatagory
 from django.shortcuts import get_object_or_404
 
+
 class CatagorySerilizer(serializers.ModelSerializer):
     class Meta:
         model=Catagory
@@ -19,32 +20,52 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model=CustomerProfile
         fields=['id','Phone_no','user']
+class OrderItemSeializer(serializers.ModelSerializer):
+   
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+  
+    class Meta:
+        model=OrderItems
+        fields=['id','product','quantity']
 class OrderSerilizer(serializers.ModelSerializer):
     customer= serializers.PrimaryKeyRelatedField(read_only=True)
+    order_items = OrderItemSeializer(many=True)
+    total = serializers.DecimalField(max_digits=8, decimal_places=2, read_only=True)
+
+    
+
     class Meta:
         model=Orders
-        fields=['orderuniqueId','customer','date_created','status','location',]
+        fields=['orderuniqueId','customer','total','date_created','status','location','order_items']
+    
+    def create(self, validated_data):
+        order_items_data = validated_data.pop('order_items')  
+        order = Orders.objects.create(**validated_data)
+        orderitemarray = []
+
+        for order_item_data in order_items_data:
+            product = order_item_data['product']
+            quantity = order_item_data['quantity']
+            data = OrderItems.objects.create(product=product, quantity=quantity)
+            orderitemarray.append(data)
+        order.order_items.set(orderitemarray)
+
+        # Calculate and set the total field
+        
+        
+        return order
+
+       
     def save(self, **kwargs):
         if self.context['userid']:
             (customer,created)=CustomerProfile.objects.get_or_create(user_id=self.context['userid'])
             if customer:
                 self.validated_data['customer'] = customer
         return super().save(**kwargs)
-class OrderItemSeializer(serializers.ModelSerializer):
-    order=serializers.PrimaryKeyRelatedField(read_only=True)
-    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
-  
-    class Meta:
-        model=OrderItems
-        fields=['id','order','product','quantity']
-    def save(self, **kwargs):
-       
-       order=get_object_or_404(Orders, orderuniqueId=self.context['order_id'])
-       if order:
-            self.validated_data['order'] = order
-       return super().save(**kwargs)
+
 class NewsSerializers(serializers.ModelSerializer):
     class Meta:
         model=News
         fields=['id','Headline','description']
+
 
